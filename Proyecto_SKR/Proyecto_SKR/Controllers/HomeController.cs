ed13024ee6bdbba6176e89863a253ae57d3f88ca
joluvapi06@gmail.com <joluvapi06@gmail.com>
2019-05-03 09:40:29 -0500

    Actualizacion del proyecto SKR

--- Proyecto_SKR/Proyecto_SKR/Controllers/HomeController.cs
+++ Proyecto_SKR/Proyecto_SKR/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Proyecto_SKR.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -29,6 +31,174 @@ namespace Proyecto_SKR.Controllers
         public ActionResult Formulario()
         {
             return View();
+        }
+        public ActionResult GuardarRegistro(String gerente_comercial, String motivo_req, String cliente_nom, String gerencia, String empresa_cliente,
+            String email_cliente, String telefono_cliente, String perfil_solicitante, String carrera_solicitante, String nivel_estudios,
+            String tiempo_experiencia, int catidad_recursos, String areas_carreras, String certificaciones, String prioridad_requerimiento,
+            DateTime fecha_sol_req, DateTime fecha_env_sol, DateTime fecha_cierre_req, String rango_edad, String genero, String idiomas,
+            String rango_tarifa, String rango_sueldo, String adicionales, String compu_trabajo, DateTime compu_trabajo_fecha, String redes_sociales,
+            DateTime redes_sociales_fecha, String pagina_web, DateTime pagina_web_fecha, String occ_mundial, DateTime occ_mundial_fecha)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=KABEC-PC\SQLEXPRESS; Initial Catalog=SolucionesKabec; User ID=sa; Password=qwerty123");
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO [Formulario] (gerente_comercial,motivo_req,cliente_nom,gerencia,empresa_cliente,
+            email_cliente,telefono_cliente,perfil_solicitante,carrera_solicitante,nivel_estudios,tiempo_experiencia,catidad_recursos,
+            areas_carreras,certificaciones,prioridad_requerimiento,fecha_sol_req,fecha_env_sol,fecha_cierre_req,rango_edad,genero,
+            idiomas,rango_tarifa,rango_sueldo,adicionales,compu_trabajo,compu_trabajo_fecha,redes_sociales,redes_sociales_fecha,
+            pagina_web,pagina_web_fecha,occ_mundial,occ_mundial_fecha) VALUES (@gerente_comercial,@motivo_req,@cliente_nom,@gerencia,@empresa_cliente,
+            @email_cliente,@telefono_cliente,@perfil_solicitante,@carrera_solicitante,@nivel_estudios,@tiempo_experiencia,@catidad_recursos,
+            @areas_carreras,@certificaciones,@prioridad_requerimiento,@fecha_sol_req,@fecha_env_sol,@fecha_cierre_req,@rango_edad,@genero,
+            @idiomas,@rango_tarifa,@rango_sueldo,@adicionales,@compu_trabajo,@compu_trabajo_fecha,@redes_sociales,@redes_sociales_fecha,
+            @pagina_web,@pagina_web_fecha,@occ_mundial,@occ_mundial_fecha)", con);
+            cmd.Parameters.Add(new SqlParameter("gerente_comercial", gerente_comercial));
+            cmd.Parameters.Add(new SqlParameter("motivo_req", motivo_req));
+            cmd.Parameters.Add(new SqlParameter("cliente_nom", cliente_nom));
+            cmd.Parameters.Add(new SqlParameter("gerencia", gerencia));
+            cmd.Parameters.Add(new SqlParameter("empresa_cliente", empresa_cliente));
+            cmd.Parameters.Add(new SqlParameter("email_cliente", email_cliente));
+            cmd.Parameters.Add(new SqlParameter("telefono_cliente", telefono_cliente));
+            cmd.Parameters.Add(new SqlParameter("perfil_solicitante", perfil_solicitante));
+            cmd.Parameters.Add(new SqlParameter("carrera_solicitante", carrera_solicitante));
+            cmd.Parameters.Add(new SqlParameter("nivel_estudios", nivel_estudios));
+            cmd.Parameters.Add(new SqlParameter("tiempo_experiencia", tiempo_experiencia));
+            cmd.Parameters.Add(new SqlParameter("catidad_recursos", catidad_recursos));
+            cmd.Parameters.Add(new SqlParameter("areas_carreras", areas_carreras));
+            cmd.Parameters.Add(new SqlParameter("certificaciones", certificaciones));
+            cmd.Parameters.Add(new SqlParameter("prioridad_requerimiento", prioridad_requerimiento));
+            cmd.Parameters.Add(new SqlParameter("fecha_sol_req", fecha_sol_req));
+            cmd.Parameters.Add(new SqlParameter("fecha_env_sol", fecha_env_sol));
+            cmd.Parameters.Add(new SqlParameter("fecha_cierre_req", fecha_cierre_req));
+            cmd.Parameters.Add(new SqlParameter("rango_edad", rango_edad));
+            cmd.Parameters.Add(new SqlParameter("genero", genero));
+            cmd.Parameters.Add(new SqlParameter("idiomas", idiomas));
+            cmd.Parameters.Add(new SqlParameter("rango_tarifa", rango_tarifa));
+            cmd.Parameters.Add(new SqlParameter("rango_sueldo", rango_sueldo));
+            cmd.Parameters.Add(new SqlParameter("adicionales", adicionales));
+            cmd.Parameters.Add(new SqlParameter("compu_trabajo", compu_trabajo));
+            cmd.Parameters.Add(new SqlParameter("compu_trabajo_fecha", compu_trabajo_fecha));
+            cmd.Parameters.Add(new SqlParameter("redes_sociales", redes_sociales));
+            cmd.Parameters.Add(new SqlParameter("redes_sociales_fecha", redes_sociales_fecha));
+            cmd.Parameters.Add(new SqlParameter("pagina_web", pagina_web));
+            cmd.Parameters.Add(new SqlParameter("pagina_web_fecha", pagina_web_fecha));
+            cmd.Parameters.Add(new SqlParameter("occ_mundial", occ_mundial));
+            cmd.Parameters.Add(new SqlParameter("occ_mundial_fecha", occ_mundial_fecha));
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return Content("Requerimiento Guardado !exito!");
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public ActionResult ConsultarRegistro()
+        {
+            List<Requerimiento> modelo = new List<Requerimiento>();
+            SqlConnection con = new SqlConnection(@"Data Source=KABEC-PC\SQLEXPRESS; Initial Catalog=SolucionesKabec; User ID=sa; Password=qwerty123");
+            SqlCommand cmd = new SqlCommand(@"SELECT * FROM [Formulario];", con);
+            try
+            {
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    modelo.Add(new Requerimiento { id_req = dr.GetInt32(0), gerente_comercial = dr.GetString(1), motivo_req = dr.GetString(2),
+                        cliente_nom = dr.GetString(3), gerencia = dr.GetString(4), empresa_cliente = dr.GetString(5), email_cliente = dr.GetString(6),
+                        telefono_cliente = dr.GetString(7), perfil_solicitante = dr.GetString(8), carrera_solicitante = dr.GetString(9),
+                        nivel_estudios = dr.GetString(10), tiempo_experiencia = dr.GetString(11), catidad_recursos = dr.GetInt32(12),
+                        areas_carreras = dr.GetString(13), certificaciones = dr.GetString(14), prioridad_requerimiento = dr.GetString(15),
+                        fecha_sol_req = dr.GetDateTime(16),  fecha_env_sol = dr.GetDateTime(17), fecha_cierre_req = dr.GetDateTime(18),
+                        rango_edad = dr.GetString(19), genero = dr.GetString(20), idiomas = dr.GetString(21), rango_tarifa = dr.GetString(22),
+                        rango_sueldo = dr.GetString(23), adicionales = dr.GetString(24), compu_trabajo= dr.GetString(25),
+                        compu_trabajo_fecha = dr.GetDateTime(26), redes_sociales = dr.GetString(27), redes_sociales_fecha = dr.GetDateTime(28),
+                        pagina_web = dr.GetString(29), pagina_web_fecha = dr.GetDateTime(30), occ_mundial = dr.GetString(31),
+                        occ_mundial_fecha = dr.GetDateTime(32)});
+                }
+                return View(modelo);
+
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public ActionResult BorrarRegistro(String gerente_comercial, String cliente_nom, String empresa_cliente, String perfil_solicitante)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=KABEC-PC\SQLEXPRESS; Initial Catalog=SolucionesKabec; User ID=sa; Password=qwerty123");
+            SqlCommand cmd = new SqlCommand(@"DELETE FROM [Formulario] WHERE gerente_comercial=@gerente_comercial AND cliente_nom=@cliente_nom
+                                            AND empresa_cliente=@empresa_cliente AND perfil_solicitante=@perfil_solicitante;", con);
+            cmd.Parameters.Add(new SqlParameter("gerente_comercial", gerente_comercial));
+            cmd.Parameters.Add(new SqlParameter("cliente_nom", cliente_nom));
+            cmd.Parameters.Add(new SqlParameter("empresa_cliente", empresa_cliente));
+            cmd.Parameters.Add(new SqlParameter("perfil_solicitante", perfil_solicitante));
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return Content("Requerimiento Eliminado !exito!");
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public ActionResult BorrarRegistro(Int32 id_req)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=KABEC-PC\SQLEXPRESS; Initial Catalog=SolucionesKabec; User ID=sa; Password=qwerty123");
+            SqlCommand cmd = new SqlCommand(@"DELETE FROM [Formulario] WHERE id_req=@id_req;", con);
+            cmd.Parameters.Add(new SqlParameter("id_req", id_req));
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return Content("Requerimiento Eliminado !exito!");
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public ActionResult ActualizarRegistro(String gerente_comercial, String cliente_nom, String empresa_cliente, String perfil_solicitante)
+        {
+            //Cuales van a ser los parametros a actualizar
+
+            SqlConnection con = new SqlConnection(@"Data Source=KABEC-PC\SQLEXPRESS; Initial Catalog=SolucionesKabec; User ID=sa; Password=qwerty123");
+            SqlCommand cmd = new SqlCommand(@"UPDATE [Formulario] set Id_farmaco=@Id_farmaco, Nombre=@Nombre where Id_farmaco=@Id_farmaco;", con);
+            cmd.Parameters.Add(new SqlParameter("gerente_comercial", gerente_comercial));
+            cmd.Parameters.Add(new SqlParameter("cliente_nom", cliente_nom));
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                return Content("Requerimiento Actualizado !exito!");
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
---
+++ Proyecto_SKR/Proyecto_SKR/Models/Requerimiento.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_SKR.Models
+{
+    public class Requerimiento
+    {
+        public Int32 id_req { get; set; }
+        public String gerente_comercial { get; set; }
+        public String motivo_req { get; set; }
+        public String cliente_nom { get; set; }
+        public String gerencia { get; set; }
+        public String empresa_cliente { get; set; }
+        public String email_cliente { get; set; }
+        public String telefono_cliente { get; set; }
+        public String perfil_solicitante { get; set; }
+        public String carrera_solicitante { get; set; }
+        public String nivel_estudios { get; set; }
+        public String tiempo_experiencia { get; set; }
+        public Int32 catidad_recursos { get; set; }
+        public String areas_carreras { get; set; }
+        public String certificaciones { get; set; }
+        public String prioridad_requerimiento { get; set; }
+        public DateTime fecha_sol_req { get; set; }
+        public DateTime fecha_env_sol { get; set; }
+        public DateTime fecha_cierre_req { get; set; }
+        public String rango_edad { get; set; }
+        public String genero { get; set; }
+        public String idiomas { get; set; }
+        public String rango_tarifa { get; set; }
+        public String rango_sueldo { get; set; }
+        public String adicionales { get; set; }
+        public String compu_trabajo { get; set; }
+        public DateTime compu_trabajo_fecha { get; set; }
+        public String redes_sociales { get; set; }
+        public DateTime redes_sociales_fecha { get; set; }
+        public String pagina_web { get; set; }
+        public DateTime pagina_web_fecha { get; set; }
+        public String occ_mundial { get; set; }
+        public DateTime occ_mundial_fecha  { get; set; }
+}
+}
